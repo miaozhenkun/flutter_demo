0a5db99b406eec134c78f815233f55669ff21c35
miao <641105850@qq.com>
2019-02-16 21:08:43 +0800

    页面切换动画测试

--- lib/views/firstPage.dart
+++ lib/views/firstPage.dart
@@ -14,6 +14,18 @@ class FirstPage extends StatefulWidget {
 }
 
 class FirstPageState extends State<FirstPage> {
+
+  //创建页面切换动画
+  SlideTransition createTransition(Animation<double> animation, Widget child) {
+    return new SlideTransition(
+      position: new Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: const Offset(0.0, 0.0),
+      ).animate(animation),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(child: new StoreBuilder<MkState>(builder: (context, store) {
@@ -76,26 +88,43 @@ class FirstPageState extends State<FirstPage> {
   }
 
   void toTsPage() {
-    Navigator.push(
-        context, new MaterialPageRoute(builder: (context) => new TsPage()));
+    //不带动画跳转
+    // Navigator.push(
+    //     context, new MaterialPageRoute(builder: (context) => new TsPage()));
+    //页面从左到右
+    Navigator.push<String>(
+        context,
+        new PageRouteBuilder(pageBuilder: (BuildContext context,
+            Animation<double> animation, Animation<double> secondaryAnimation) {
+          // 跳转的路由对象
+          return new TsPage();
+        }, transitionsBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+          Widget child,
+        ) {
+          return createTransition(animation, child);
+        }));
   }
 
   void toXsPage() {
     Navigator.push(context,
         new MaterialPageRoute(builder: (context) => new XsDetailPage()));
   }
-  void toVideoPage(){
-        Navigator.push(context,
-        new MaterialPageRoute(builder: (context) => new videoPage()));
+
+  void toVideoPage() {
+    Navigator.push(
+        context, new MaterialPageRoute(builder: (context) => new videoPage()));
   }
 
   //切换夜间模式
-  void switchTheme(context, store) async{
+  void switchTheme(context, store) async {
     var list = ['白天', '夜间'];
     var sp = await SpUtil.getInstance();
     CommonUtils.showCommitOptionDialog(context, list, (index) {
-      sp.putInt('theme', index+1);
-      CommonUtils.pushTheme(store, index+1);
+      sp.putInt('theme', index + 1);
+      CommonUtils.pushTheme(store, index + 1);
       //LocalStorage.save(Config.THEME_COLOR, index.toString());
     }, colorList: CommonUtils.getThemeListColor());
   }
